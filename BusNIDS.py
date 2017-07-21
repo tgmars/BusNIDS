646168f5ad7176a954194d2a9bfe10c39d0030ad
Tom Marsden <tomgmarsden@gmail.com>
2017-07-21 02:09:17 +0000

    Basic IDS engine added

---
+++ BusNIDS.py
@@ -0,0 +1,32 @@
+from scapy.all import *
+
+load_contrib('modbus')
+
+MAX_PACKETS = 4
+PORT = '502'
+INTERFACE = "wlan0"
+
+packetCount = -1
+
+def customDisplay(packet):
+	if packet.haslayer(ModbusADURequest):
+		global packetCount
+		packetCount += 1
+		print packetCount
+		#Used to generate a visualisation of the sniffed packet as a .pdf
+		#packet[packetCount].pdfdump('packet.pdf')
+
+		#Uncomment to present more details of the sniffed packet to the console.
+		#return packet[packetCount].show()
+		if "error" in packet:
+			return 'src {} -> dst {} {} -> Likely malformed packet'.format(packet[packetCount][2].src, packet[packetCount][2].dst, packet.getlayer())
+		else:
+			return "valid modbus traffic"
+	else: 
+		return "not modbus traffic" 
+
+## Configure the sniff scapy argument for port 502 on the Rpi wireless interface and only sniff MAX_PACKETS  packets.
+
+pkt=sniff(filter="port "+PORT, iface="wlan0", count = MAX_PACKETS,  prn=customDisplay)
+
+
