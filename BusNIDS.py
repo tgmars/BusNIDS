2a868edfd6736f4c84d7eda667a8d979b2f03ad0
Tom Desktop <tomgmarsden@gmail.com>
2017-07-23 22:12:52 +1000

    Added Response Detection

--- BusNIDS.py
+++ BusNIDS.py
@@ -16,7 +16,12 @@ INTERFACE = "wlan0"
 packetCount = -1
 
 def customDisplay(packet):
-	if packet.haslayer(ModbusADURequest):
+
+	#TODO: Add statistics for each valid type of packet
+
+	#Writing to Holding Registers Request - Not actually an ADURequest but a ModbusPDU10WriteMultipleRegistersRequest
+
+	if packet.haslayer(ModbusADURequest): #Change this so that there is a list of 'layers' and if incldued then execute.
 		global packetCount
 		packetCount += 1
 		print packetCount
@@ -29,11 +34,26 @@ def customDisplay(packet):
 			return 'src {} -> dst {} {} -> Likely malformed packet'.format(packet[packetCount][2].src, packet[packetCount][2].dst, packet.getlayer())
 		else:
 			return "Valid ModbusADURequest"
+
+	if packet.haslayer(ModbusADUResponse): #Change this so that there is a list of 'layers' and if incldued then execute.
+		global packetCount
+		packetCount += 1
+		print packetCount
+		#Used to generate a visualisation of the sniffed packet as a .pdf
+		#packet[packetCount].pdfdump('packet.pdf')
+
+		#Uncomment to present more details of the sniffed packet to the console.
+		#return packet[packetCount].show()
+		if "error" in packet:
+			return 'src {} -> dst {} {} -> Likely malformed packet'.format(packet[packetCount][2].src, packet[packetCount][2].dst, packet.getlayer())
+		else:
+			return "Valid ModbusADUResponse"
+
 	else: 
 		return "not modbus traffic" 
 
 ## Configure the sniff scapy argument for port 502 on the Rpi wireless interface and only sniff MAX_PACKETS  packets.
 
-pkt=sniff(filter="port "+PORT, iface="wlan0", count = MAX_PACKETS,  prn=customDisplay)
+pkt=sniff(filter="port "+PORT, iface=INTERFACE, count = MAX_PACKETS,  prn=customDisplay)
 
 
