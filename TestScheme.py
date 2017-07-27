a7b28d4521511d5c9a10681d0974355661f83e1d
Tom Laptop <tomgmarsden@gmail.com>
2017-07-27 11:44:18 +1000

    Added error packet to send.

--- TestScheme.py
+++ TestScheme.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#  v0.0.1
+#  v0.0.2
 
 from scapy.all import *
 import argparse
@@ -8,7 +8,7 @@ import argparse
 load_contrib('modbus')
 
 #Parse arguments from command line at runtime
-parser = argparse.ArgumentParser(description="Sends a variety of valid and invalid Modbus TCP packets to a user-defined IP address to test robustness of a PLC or IDS.")
+parser = argparse.ArgumentParser(description="Sends a variety of valid and invalid Modbus TCP packets from an 'attacker' to a user-defined IP address to test robustness of a PLC or IDS.")
 parser.add_argument("dst_ip", help="IP address of Modbus PLC or IDS to test.")
 parser.add_argument("dst_port", type=int, help="Port that the Modbus TCP service is running on the destination machine.")
 args=parser.parse_args()
@@ -19,3 +19,7 @@ testSock.connect((args.dst_ip,502))
 testStream = StreamSocket(testSock)
 
 #Create various packets to send to the destination
+errRegistersPacket=ModbusADURequest()/ModbusPDU10WriteMultipleRegistersRequest(startingAddr=1,outputsValue=[13],quantityRegisters='2L')
+
+#Send the desired packets
+response=testStream.sr(errRegistersPacket)
